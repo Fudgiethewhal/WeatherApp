c7177669b056648a5e26a26c030769e2b0fd2645
Bryseida Hernandez <bryseida.hernandez@yahoo.com>
2025-04-07 22:07:07 -0700

    adding features

---
+++ WeatherApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using Newtonsoft.Json.Linq;
+using System.Text.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WeatherApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string apiKey = "973db427debb88e41756d8f9de4f4487"; 
+        string weatherURL = $"https://api.openweathermap.org/data/2.5/weather?zip=97527,us&units=imperial&appid={apiKey}";
+
+        using var client = new HttpClient();
+        var response = client.GetStringAsync(weatherURL).Result;
+
+        var weather = JsonDocument.Parse(response);
+        var temp = weather.RootElement.GetProperty("main").GetProperty("temp").GetDouble();
+        var description = weather.RootElement.GetProperty("weather")[0].GetProperty("description").GetString();
+
+        Console.WriteLine($"\n🌤️  Grants Pass, OR Weather"); 
+        Console.WriteLine($"Temperature: {temp}°F");
+        Console.WriteLine($"Condition: {description}");
+      
+        string art = description.Contains("cloud") ? "☁️"
+                    : description.Contains("rain") ? "🌧️"
+                    : "☀️";
+        Console.WriteLine($"\n{art}  {description.ToUpper()} {art}");
+
+        if (temp < 50)
+            Console.ForegroundColor = ConsoleColor.Cyan;
+        else if (temp < 80) 
+            Console.ForegroundColor = ConsoleColor.Yellow;
+        else if (temp > 80)
+            Console.ForegroundColor = ConsoleColor.Red;
+
+        Console.WriteLine($"🌡️ It's currently {temp}°F in Grants Pass.");
+        Console.ResetColor();
+
+    }
+}
